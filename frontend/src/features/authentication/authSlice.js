7534aeed01915c32bfa2971be13c6cba607b7b35
VladimirMastepanov <rozenkranc4@gmail.com>
2024-07-13 13:10:06 +0200

    add frontend/src/features/authentication/authSlice.js

---
+++ frontend/src/features/authentication/authSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { user: null, token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+export const selectCurrentUser = (state) => state.auth.user;
+export default authSlice.reducer;
