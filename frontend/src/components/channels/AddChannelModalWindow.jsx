e1122a842d623271bca357700ef841e92533031e
VladimirMastepanov <rozenkranc4@gmail.com>
2024-07-16 21:52:06 +0200

    add  src/components/channels/AddChannelModalWindow.jsx

---
+++ frontend/src/components/channels/AddChannelModalWindow.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+import { useFormik } from 'formik';
+import { Modal, FormGroup, FormControl, Button } from 'react-bootstrap';
+
+const AddChannelModalWindow = (props) => {
+  const { onHide, show } = props;
+  const inputRef = useRef();
+
+  const f = useFormik({ onSubmit: console.log(props), initialValues: { body: '' } });
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>Добавить канал</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <form onSubmit={f.handleSubmit}>
+          <FormGroup>
+            <FormControl
+              className="mb-2 form-control"
+              required
+              ref={inputRef}
+              onChange={f.handleChange}
+              onBlur={f.handleBlur}
+              value={f.values.body}
+              data-testid="input-body"
+              name="body"
+            />
+          </FormGroup>
+          <div className="d-flex justify-content-end">
+            <Button type="button" className="me-2 btn btn-secondary" onClick={onHide}>Отменить</Button>
+            <Button type="submit" className="btn btn-primary">Отправить</Button>
+          </div>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default AddChannelModalWindow;
--- frontend/src/components/channels/ChannelsColumn.jsx
+++ frontend/src/components/channels/ChannelsColumn.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChannelsList from './ChannelsList';
+import AddChannelModalWindow from './AddChannelModalWindow';
 
 const ChannelsColumn = ({ activeChannelName, setActiveChannelName }) => {
-  const q = 1;
+  const [showModal, setShowModal] = useState(false);
+
+  const onHide = () => setShowModal(false)
 
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between h-100 mb-2 ps-4 pe-2 p-4">
         <b>Каналы</b>
-        <button type="button" className="p-0 text-primary btn btn-group-vertical">
+        <button onClick={() => setShowModal(true)} type="button" className="p-0 text-primary btn btn-group-vertical">
           <svg xmlns="https:/www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
@@ -20,6 +23,7 @@ const ChannelsColumn = ({ activeChannelName, setActiveChannelName }) => {
         activeChannelName={activeChannelName}
         setActiveChannelName={setActiveChannelName}
       />
+      <AddChannelModalWindow show={showModal} onHide={onHide} />
     </div>
   );
 };
