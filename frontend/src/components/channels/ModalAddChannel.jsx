e050af608493131167671a18c5c5025aa8cedd5d
VladimirMastepanov <rozenkranc4@gmail.com>
2024-07-26 22:06:55 +0200

    work on modal addChannel Window

--- frontend/src/components/channels/ModalAddChannel.jsx
+++ frontend/src/components/channels/ModalAddChannel.jsx
@@ -1,65 +1,85 @@
 import React, { useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { useFormik } from 'formik';
-import {
-  Modal,
-  FormGroup,
-  FormControl,
-  Button,
-} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Modal, Button } from 'react-bootstrap';
 import { toast } from 'react-toastify';
-import { addChannel } from '../../features/channels/channelsSlice';
+import * as yup from 'yup';
+import { addChannel, channelsSelector } from '../../features/channels/channelsSlice';
 
-const ModalAddChannel = (props) => {
+const validationSchema = yup.object().shape({
+  name: yup.string().min(3, 'От 3 до 20 символов').max(20, 'От 3 до 20 символов').required('Обязательное поле'),
+});
+
+const ModalAddChannel = ({ onHide, show }) => {
+  // const [error, setError] = useState(null);
   const dispatch = useDispatch();
-  const { onHide, show } = props;
   const inputRef = useRef();
-
-  const f = useFormik({
-    onSubmit: () => {
-      const token = localStorage.getItem('token');
-      const newChannel = {
-        name: f.values.body,
-      };
-      f.resetForm();
-      dispatch(addChannel({ token, newChannel }));
-      toast.success('Канал создан');
-      onHide();
-    },
-    initialValues: { body: '' },
-  });
+  const entities = useSelector(channelsSelector.selectEntities);
+  const channelsNames = Object.values(entities).map((channel) => channel.name);
 
   useEffect(() => {
+    if (channelsNames.length > 0) {
+      console.log(channelsNames);
+    }
     if (inputRef.current) {
       inputRef.current.focus();
     }
-  }, []);
+  }, [channelsNames]);
 
   return (
-    <Modal show={show} onHide={onHide}>
+    <Modal show={show} onHide={onHide} className="modal-dialog-centered">
       <Modal.Header closeButton>
         <Modal.Title>Добавить канал</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <form onSubmit={f.handleSubmit}>
-          <FormGroup>
-            <FormControl
-              className="mb-2 form-control"
-              required
-              ref={inputRef}
-              onChange={f.handleChange}
-              onBlur={f.handleBlur}
-              value={f.values.body}
-              data-testid="input-body"
-              name="body"
-            />
-          </FormGroup>
-          <div className="d-flex justify-content-end">
-            <Button type="button" className="me-2 btn btn-secondary" onClick={onHide}>Отменить</Button>
-            <Button type="submit" className="btn btn-primary">Отправить</Button>
-          </div>
-        </form>
+
+        <Formik
+          initialValues={{ name: '' }}
+          validationSchema={validationSchema}
+          onSubmit={async (values, actions) => {
+            console.log(channelsNames);
+            if (!channelsNames.includes(values.name)) {
+              // setError(null);
+              const token = localStorage.getItem('token');
+              const newChannel = { name: values.name };
+              try {
+                await dispatch(addChannel({ token, newChannel }));
+                toast.success('Канал создан');
+                actions.resetForm();
+                onHide();
+              } catch (e) {
+                toast.error(e.message);
+              }
+            } else {
+              actions.setFieldError('name', 'Должно быть уникальным');
+            }
+            actions.setSubmitting(false);
+          }}
+        >
+          {(props) => (
+            <Form onSubmit={props.handleSubmit}>
+              <Field
+                innerRef={inputRef}
+                required
+                name="name"
+                id="name"
+                value={props.values.name}
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                className={`mb-2 form-control ${props.errors.name ? 'is-invalid' : ''}`}
+              />
+              <label className="visually-hidden" htmlFor="name">Имя канала</label>
+              <ErrorMessage name="name" component="div" className="invalid-feedback" />
+              <div className="d-flex justify-content-end">
+                <Button type="button" className="me-2 btn btn-secondary" onClick={onHide} disabled={props.isSubmitting}>Отменить</Button>
+                <Button type="submit" className="btn btn-primary" disabled={props.isSubmitting}>Отправить</Button>
+              </div>
+            </Form>
+          )}
+
+        </Formik>
+
       </Modal.Body>
     </Modal>
   );
