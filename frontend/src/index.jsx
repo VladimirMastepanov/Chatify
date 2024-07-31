ed2cc8187c53b643f78347ca2a9dd080db45c2a6
VladimirMastepanov <rozenkranc4@gmail.com>
2024-07-31 18:22:08 +0200

    add rollbar

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,25 +3,34 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {
+  ErrorBoundary,
+  Provider as RollbarProvider,
+} from '@rollbar/react';
 import store from './app/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import rollbar from './rollbar';
 
 import './i18next';
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        pauseOnFocusLoss
-      />
-      <App />
-    </Provider>
+    <RollbarProvider instance={rollbar}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            pauseOnFocusLoss
+          />
+          <App />
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   </React.StrictMode>,
 );
 
