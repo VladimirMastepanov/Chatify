a250e55daead85133e914cc255b327e1a815ba9f
VladimirMastepanov <rozenkranc4@gmail.com>
2024-07-31 18:45:33 +0200

    add Environment Variables in render

--- frontend/src/rollbar.js
+++ frontend/src/rollbar.js
@@ -2,7 +2,7 @@ import Rollbar from 'rollbar';
 
 const rollbar = new Rollbar({
   accessToken: '6f9323de3cee497f9d6c4b9656255523',
-  environment: 'testenv',
+  environment: process.env.NODE_ENV || 'development',
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
