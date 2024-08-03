18afb0fcb9730949cc2fe3ac39335d97f3b499f1
VladimirMastepanov <rozenkranc4@gmail.com>
2024-08-03 18:23:16 +0200

    change rollbar token

--- frontend/src/rollbar.js
+++ frontend/src/rollbar.js
@@ -1,7 +1,7 @@
 import Rollbar from 'rollbar';
 
 const rollbar = new Rollbar({
-  accessToken: '6f9323de3cee497f9d6c4b9656255523',
+  accessToken: '96083c626081455c9229aa55b1590701',
   environment: process.env.NODE_ENV || 'development',
   captureUncaught: true,
   captureUnhandledRejections: true,
