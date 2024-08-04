19ba8216656e2b3e7ed64c2a9e883e5903212ba1
VladimirMastepanov <rozenkranc4@gmail.com>
2024-08-04 21:09:58 +0200

    delete base url in routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,10 @@
-const getApiUrl = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.REACT_APP_API_URL || '';
-  }
-  return '';
-};
-
-const apiUrl = getApiUrl();
 const apiPath = '/api/v1';
 
 export default {
-  loginPath: () => [apiUrl, apiPath, 'login'].join('/'),
-  signUpPath: () => [apiUrl, apiPath, 'signup'].join('/'),
-  channelsPath: () => [apiUrl, apiPath, 'channels'].join('/'),
-  channelPathWithId: (id) => [apiUrl, apiPath, `channels/${id}`].join('/'),
+  loginPath: () => [apiPath, 'login'].join('/'),
+  signUpPath: () => [apiPath, 'signup'].join('/'),
+  channelsPath: () => [apiPath, 'channels'].join('/'),
+  channelPathWithId: (id) => [apiPath, `channels/${id}`].join('/'),
   messagesPath: () => [apiPath, 'messages'].join('/'),
-  messagesPathWithId: (id) => [apiUrl, apiPath, `messages/${id}`].join('/'),
+  messagesPathWithId: (id) => [apiPath, `messages/${id}`].join('/'),
 };
