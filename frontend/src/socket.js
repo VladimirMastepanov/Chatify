bef512cf5646bc1a5cf748e10a546357dee5f999
VladimirMastepanov <rozenkranc4@gmail.com>
2024-08-07 21:25:26 +0200

    fix socket settings

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,16 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.NODE_ENV === 'production'
-  ? 'wss://frontend-project-1-2y5w.onrender.com'
-  : 'ws://localhost:5001', {
-  transports: ['websocket'],
-  reconnection: true,
-  reconnectionAttempts: Infinity,
-  reconnectionDelay: 1000,
-  reconnectionDelayMax: 5000,
-  randomizationFactor: 0.5,
-  pingInterval: 25000,
-  pingTimeout: 60000,
-});
+const socket = io();
 
 export default socket;
