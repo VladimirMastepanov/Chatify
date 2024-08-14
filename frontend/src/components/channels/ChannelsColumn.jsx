d29e98cbbf4f2a0df7a5655d75a060770b10a9f0
VladimirMastepanov <rozenkranc4@gmail.com>
2024-08-14 15:33:45 +0200

    add SvgArrowIcon.jsx and SvgPlusIcon.jsx

--- frontend/src/components/channels/ChannelsColumn.jsx
+++ frontend/src/components/channels/ChannelsColumn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ModalAddChannel from './ModalAddChannel';
 import ChannelsList from './ChannelsList';
+import SvgPlusIcon from '../SvgPlusIcon';
 
 const ChannelsColumn = () => {
   const [showModal, setShowModal] = useState(false);
@@ -15,10 +16,7 @@ const ChannelsColumn = () => {
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('channels')}</b>
         <button onClick={() => setShowModal(true)} type="button" className="p-0 text-primary btn btn-group-vertical">
-          <svg xmlns="https:/www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-          </svg>
+          <SvgPlusIcon />
           <span className="visually-hidden">{t('plus')}</span>
         </button>
       </div>
